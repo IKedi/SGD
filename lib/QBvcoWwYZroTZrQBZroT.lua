eeb15684dc2a5a19e19beacbb9054e3bdacb28a6
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-23 01:01:19 +0300

    Add files via upload

---
+++ lib/QBvcoWwYZroTZrQBZroT.lua
@@ -0,0 +1,6 @@
+--Multiplier:doesn't work in this game.,AutoSell:false,By:IKedi--
+local rem = game.Players.LocalPlayer.ninjaEvent
+while wait() do
+	if _G.sgd_kill then return end
+	rem:FireServer("swingKatana")
+end

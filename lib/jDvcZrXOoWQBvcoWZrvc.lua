b87c016e8cbe91b22d61ba37e978f8397c893d4e
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-23 04:09:01 +0300

    Update jDvcZrXOoWQBvcoWZrvc.lua

--- lib/jDvcZrXOoWQBvcoWZrvc.lua
+++ lib/jDvcZrXOoWQBvcoWZrvc.lua
@@ -1,10 +1,16 @@
 --Multiplier:30,AutoSell:true,By:IKedi--
+
 while wait() do
 	if _G.sgd_kill then return end
 	for i = 1, tonumber(_G.multiplier) do
 		game:GetService("ReplicatedStorage").DrinksGives.Give18:FireServer()
 	end
 	if _G.sgd_autosell then
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-4920.07861, 3764.83008, -1057.08215, -0.137502, -5.09118259e-08, 0.990501463, -7.72833104e-08, 1, 4.06715337e-08, -0.990501463, -7.09568155e-08, -0.137502))
+		if math.random(0, 50) == 1 then
+            local lastpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-4920.15576, 3764.83203, -1058.12317, -0.341552198, 4.25243201e-08, -0.939862788, 1.07960993e-07, 1, 6.01151973e-09, 0.939862788, -9.94152742e-08, -0.341552198)
+            wait()
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = lastpos
+        end
 	end
 end

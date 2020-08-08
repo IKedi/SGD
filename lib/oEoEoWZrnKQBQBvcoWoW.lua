251175dea863fc7c1219943ad2ed2413dd252ad2
IKedi <36607876+IKedi@users.noreply.github.com>
2020-08-08 03:20:19 +0300

    Update oEoEoWZrnKQBQBvcoWoW.lua

--- lib/oEoEoWZrnKQBQBvcoWoW.lua
+++ lib/oEoEoWZrnKQBQBvcoWoW.lua
@@ -23,7 +23,7 @@ while wait() do
 		    if _1 == _2 then
 		       local lpos =  plr.Character.HumanoidRootPart.CFrame
 		       plr.Character.HumanoidRootPart.CFrame = CFrame.new(-79.1455536, 51.0369606, -91.2797852, 0.829343677, 5.03827451e-08, -0.558738828, -4.25581383e-08, 1, 2.70026366e-08, 0.558738828, 1.38441958e-09, 0.829343677)
-		       wait()
+		       wait(0)
 		       plr.Character.HumanoidRootPart.CFrame = lpos
 		    end
 	    end
